585afd1a901ffc0907025c9b516609cd2ef16695
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-08-14 09:18:36 -0500

    Pruebas con hash chaining

--- Propuestos/Test.java
+++ Propuestos/Test.java
@@ -1,19 +1,27 @@
 package Propuestos;
-import java.util.LinkedList;
 public class Test {
     public static void main(String[] args) {
         
         HashChaining hash = new HashChaining();
-        hash.put("arrecho", 11);
-        hash.put("aanatomia", 145);
-        hash.put("vibora", 12);
-        hash.put("costra", 14);
-        hash.put("diente", 164);
+        hash.put("a", 11);
+        hash.put("b", 145);
+        hash.put("c", 12);
+        hash.put("d", 14);
+        hash.put("e", 164);
+        hash.put("f", 112164);
+        hash.put("g", 163234);
+        hash.put("h", 16444);
+        hash.put("i", 142424);
+        hash.put("j", 4234);
+        hash.put("k", 15364);
+        hash.put("l", 111144);
+        hash.put("b", 15564);
+        hash.put("a", 111434);
 
         System.out.println(hash);
         System.out.println(hash.size());
-        System.out.println(hash.get("aanatomia"));
-        System.out.println(hash.remove("vibora"));
+        System.out.println(hash.get("i"));
+        hash.remove("i");
         System.out.println(hash);
     }
 
